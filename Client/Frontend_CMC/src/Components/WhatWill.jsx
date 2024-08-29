7bfacc8f836ab4590fa3f003bba27cbb97c25462
Debmalya Saha <130841563+debmalya01@users.noreply.github.com>
2024-08-29 21:57:08 +0530

    Add files via upload

---
+++ Client/Frontend_CMC/src/Components/WhatWill.jsx
@@ -0,0 +1,40 @@
+function WhatWill(){
+    return(
+        <>
+            <div className="px-24 pb-11 mt-36">
+                <div className="py-16 mb-10 flex items-center gap-24">
+                    <p>3/ What will</p>
+                    <div className="bg-black h-[0.6px] w-40"></div>
+                    <p className="text-7xl font-bold ">You Achieve</p>
+                </div>
+                <div className="flex gap-11 w-full mt-20">
+                        <div className=" border-[1px] border-black p-8 rounded-full">
+                            <p>Get Ceritified with</p>
+                            <p>Govt. ISO Certified Courses</p>
+                        </div>
+                        <div className=" border-[1px] border-b-[16px] p-8 border-black rounded-full">
+                            <p>Handson experience</p>
+                            <p>Build real world projects and tune up your resume</p>
+                        </div>
+                        <div className=" border-[1px] border-black p-8 rounded-full bg-orange-500">
+                            <p>Validated Skills</p>
+                            <p>Exams conducted at regular intervals to validate your skillset</p>
+                        </div>
+                </div>
+                <div className="flex gap-11 w-full mt-5 ml-10">
+                        <div className=" border-[1px] p-8 border-black rounded-full bg-sky-300">
+                            <p>Guaranteed Placements</p>
+                            <p>100% placement on referred courses</p>
+                        </div>
+                        <div className=" border-2 border-b-[16px] p-8 rounded-full border-black">
+                            <p>Career Guide</p>
+                            <p>Free Course suggestion and career guidance from counsellor</p>
+                        </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default WhatWill
